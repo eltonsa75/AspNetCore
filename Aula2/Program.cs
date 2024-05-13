0328d7ea4690bd169d9c5311e16480c56051ea41
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-13 11:24:05 -0300

    Entendimento sobre Middlewares

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -1,20 +1,50 @@
 ﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace Aula2 {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args) 
+        {
 
-            var host = new WebHostBuilder()
-            .UseKestrel()
-            .Configure(app => {
-                app.Run(context => context.Response.WriteAsync("Bem vindo ao curso"));
-            })
-            .Build();
-            host.Run();
-            
+          var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();            
         }
     }
+
+    internal class Startup {
+
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+         {
+            _configuration = configuration;
+        }
+       public void Configure(IApplicationBuilder app) 
+{
+    app.Use(async (context, next) => {
+        // Antes de manipular a requisição...
+
+        await next(); // Chama o próximo middleware na cadeia
+
+        // Depois de receber uma resposta do próximo middleware...
+        await context.Response.WriteAsync("Bem vindo 1 ");
+    });
+
+    app.Use(async (context, next) => {
+        // Antes de manipular a requisição...
+
+        await next(); // Chama o próximo middleware na cadeia
+
+        // Depois de receber uma resposta do próximo middleware...
+        await context.Response.WriteAsync("Bem vindo 2");
+    });
 }
+
+        }
+    }
