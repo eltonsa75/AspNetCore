1b75d5e3a6e0212a3266582508a050f5996a69e2
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-13 09:48:42 -0300

    Criando um Hosting no Asp.Net

---
+++ Aula2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Aula2 {
+    class Program {
+        static void Main(string[] args) {
+
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .Configure(app => {
+                app.Run(context => context.Response.WriteAsync("Bem vindo ao curso"));
+            })
+            .Build();
+            host.Run();
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 # O QUE APREENDI NO CURSO 
 ASP.NET Core é o novo framework open-source e cross-platform para construir aplicações modernas. ASP.NET Core pode executar sobre .NET Core e .NET Framework. Tem uma melhor performance sobre a plataforma antiga e pode ser desenvolvida a partir de qualquer editor de código fonte e em qualquer sistema operacional.
 
-
-
 ![Alt text](Asp01.png)
+
+HOST
+
+![Alt text](host.png)
 
 
 
