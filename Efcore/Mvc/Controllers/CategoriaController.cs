2c462ec2820ccba30c815eb85382e8dcb06c728e
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-05-21 14:52:54 -0300

    Salvando uma categoria na entidade.

--- Efcore/Mvc/Controllers/CategoriaController.cs
+++ Efcore/Mvc/Controllers/CategoriaController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dados;
 using Dominio.Entidades;
+using Microsoft.AspNetCore.Identity.UI.Pages.Internal.Account;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Mvc.Controllers
-{
+{ 
     public class CategoriaController : Controller
     {
+
+           private readonly ApplicationDbContext _context;
+
+    public CategoriaController(ApplicationDbContext context)
+     {
+       _context = context;
+    }
 
         [HttpGet]
         public IActionResult Salvar() {
@@ -19,8 +28,10 @@ namespace Mvc.Controllers
         }
 
     [HttpPost]
-          public IActionResult Salvar(Categoria categoria) {
+          public async Task<IActionResult> Salvar(Categoria categoria) {
 
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
             return View();
         }
     }
